020ac18c9799df6f31596d582cbf2855177b1a4d
Josh Masen <joshuamasen7@gmail.com>
2021-11-30 10:12:01 -0700

    Add emoji favicon

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -7,7 +7,10 @@
     />
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😎</text></svg>"
+    />
 
     <!-- https://csswizardry.com/2020/05/the-fastest-google-fonts/ -->
     <!-- Warm up the fonts origin -->
