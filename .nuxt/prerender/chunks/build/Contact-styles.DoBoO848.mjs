4f39d8ff2f8e49721b1fd5bd28856e61bc4d9c20
Josh Masen <josh@useworkshop.com>
2024-06-21 10:29:15 -0700

    Update packages and add .nuxt to gitignore

--- .nuxt/prerender/chunks/build/Contact-styles.DoBoO848.mjs
+++
@@ -1,6 +0,0 @@
-import { C as Contact_vue_vue_type_style_index_0_scoped_438bc068_lang } from './Contact-styles-1.mjs-CrfvPRKH.mjs';
-
-const ContactStyles_DoBoO848 = [Contact_vue_vue_type_style_index_0_scoped_438bc068_lang, Contact_vue_vue_type_style_index_0_scoped_438bc068_lang];
-
-export { ContactStyles_DoBoO848 as default };
-//# sourceMappingURL=Contact-styles.DoBoO848.mjs.map
--- .nuxt/prerender/chunks/build/Footer-styles.iLrFrNwR.mjs
+++
@@ -1,6 +0,0 @@
-import { F as Footer_vue_vue_type_style_index_0_scoped_97f880f7_lang } from './Footer-styles-1.mjs-CXgiBE06.mjs';
-
-const FooterStyles_iLrFrNwR = [Footer_vue_vue_type_style_index_0_scoped_97f880f7_lang, Footer_vue_vue_type_style_index_0_scoped_97f880f7_lang];
-
-export { FooterStyles_iLrFrNwR as default };
-//# sourceMappingURL=Footer-styles.iLrFrNwR.mjs.map
