cfd12a2045083e96ea2d38ed1018a7c12f0a8530
Josh Masen <joshuamasen7@gmail.com>
2025-04-28 06:25:40 -0700

    Update bio

--- pages/index.vue
+++ pages/index.vue
@@ -7,16 +7,34 @@
       </h1>
     </header>
     <main class="max-w-lg px-8 pt-0 pb-16 m-auto">
-      <section class="leading-7">
-        <p class="mb-5">
-          I've been building stuff on the web for over 10 years. While I was in
-          college studying graphic design, I taught myself to code and started
-          building websites for freelance clients. By the time I earned my
-          degree, I realized I was super passionate about writing code and
-          creating on the web, so I started my career in web&nbsp;development.
+      <section class="flex flex-col leading-7 gap-5">
+        <p>
+          I’ve been building stuff on the web for a long time. My first client
+          was a family friend who needed a website for her at-home nursing
+          business. I think I was 12 years old. Fast forward to college: while
+          studying graphic design, I was still teaching myself to code and
+          freelancing on the side. By the time I earned my degree, I realized I
+          still had a passion for building things on the web, so I jumped into
+          web development full-time.
+        </p>
+        <p>
+          I spent the early years of my career at design agencies and larger
+          companies, where the overlap of my design education and tech
+          background led me to projects in branding, advertising, and ecommerce.
+          Eventually, I got curious about startups and entrepreneurship and
+          joined an early-stage startup as employee #1. As is typical in
+          startups, I went wherever I could provide value—first in tech support,
+          then in marketing, and ultimately into full-on software engineering.
+        </p>
+        <p>
+          After that company was acquired, I realized how much I missed building
+          software in the scrappy early stages. That’s when I joined Workshop as
+          an early engineer, helping build an email-first internal
+          communications platform. And that’s where I am today—still deeply
+          passionate about building cool stuff on the web.
         </p>
 
-        <p class="mb-2">
+        <p>
           Here's what I've been up to since then, in resume-style reverse
           chronological order:
         </p>
